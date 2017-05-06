8c27c6c5222ffe17b268d56f9e702c6a573b368f
S4M37 <sametoussema@gmail.com>
2017-05-06 12:35:40 +0200

    add elastic login credential

--- src/lib/elastic.js
+++ src/lib/elastic.js
@@ -5,7 +5,7 @@ module.exports = {
         function createInstance() {
             var elasticsearch = require('elasticsearch');
             return new elasticsearch.Client({
-                host: 'http://ec2-34-248-57-210.eu-west-1.compute.amazonaws.com:9200',
+                host: 'http://elastic:changeme@ec2-52-18-65-50.eu-west-1.compute.amazonaws.com:9200/',
                 log: 'trace'
             });
         }
